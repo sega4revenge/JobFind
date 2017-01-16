15eca6fd4632a54234c49f36e74a7e52fb2b6bd8
sontho <sontho22@Gmail.com>
2017-01-16 18:11:54 +0700

    tho

--- app/src/main/java/com/finger/jobfind/pref/PrefManager.java
+++ app/src/main/java/com/finger/jobfind/pref/PrefManager.java
@@ -32,7 +32,7 @@ public class PrefManager {
     }
 
     public boolean isFirstTimeLaunch() {
-        return pref.getBoolean(IS_FIRST_TIME_LAUNCH, true);
+        return pref.getBoolean(IS_FIRST_TIME_LAUNCH, false);
     }
 
 }
