1039a9e3c940c5f015ef9f46018fc442e0f5c82e
sontho <sontho22@Gmail.com>
2017-01-17 10:15:57 +0700

    tho

--- app/src/main/java/com/finger/jobfind/activity/IntroActivity.java
+++ app/src/main/java/com/finger/jobfind/activity/IntroActivity.java
@@ -22,6 +22,7 @@ public class IntroActivity extends AppCompatActivity {
 
     private static final int REQUEST_CODE = 1234;
     private PrefManager prefManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,20 +30,20 @@ public class IntroActivity extends AppCompatActivity {
         prefManager = new PrefManager(this);
         if (!prefManager.isFirstTimeLaunch()) {
             launchHomeScreen();
-        }
-        else
-        {
+        } else {
             startActivity(new Intent(IntroActivity.this, LoginActivity.class));
             finish();
         }
 
 
     }
+
     public void launchHomeScreen() {
 
-            Intent mainAct = new Intent(this, MaterialTutorialActivity.class);
-            mainAct.putParcelableArrayListExtra(MaterialTutorialActivity.MATERIAL_TUTORIAL_ARG_TUTORIAL_ITEMS, getTutorialItems(this));
-            startActivityForResult(mainAct, REQUEST_CODE);
+        Intent mainAct = new Intent(this, MaterialTutorialActivity.class);
+        mainAct.putParcelableArrayListExtra(MaterialTutorialActivity.MATERIAL_TUTORIAL_ARG_TUTORIAL_ITEMS, getTutorialItems(this));
+        startActivityForResult(mainAct, REQUEST_CODE);
+        finish();
 
 
     }
@@ -52,13 +53,13 @@ public class IntroActivity extends AppCompatActivity {
                 R.color.slide_1, R.drawable.find_slide_1_front, R.drawable.find_slide_1_background);
 
         TutorialItem tutorialItem2 = new TutorialItem(R.string.slide_2_tile_saving_time, R.string.slide_2_volunteer_professionals_subtitle,
-                R.color.slide_2,  R.drawable.find_slide_2_background,  R.drawable.find_slide_2_front);
+                R.color.slide_2, R.drawable.find_slide_2_background, R.drawable.find_slide_2_front);
 
         TutorialItem tutorialItem3 = new TutorialItem(R.string.slide_3_title_notification, R.string.slide_3_download_and_go_subtitle,
-                R.color.slide_3, R.drawable.find_slide_3_background,  R.drawable.find_slide_3_front);
+                R.color.slide_3, R.drawable.find_slide_3_background, R.drawable.find_slide_3_front);
 
         TutorialItem tutorialItem4 = new TutorialItem(R.string.slide_4_title_start, R.string.slide_4_different_languages_subtitle,
-                R.color.slide_4,  R.drawable.find_slide_4_front, R.drawable.find_slide_4_background);
+                R.color.slide_4, R.drawable.find_slide_4_front, R.drawable.find_slide_4_background);
 
         ArrayList<TutorialItem> tutorialItems = new ArrayList<>();
         tutorialItems.add(tutorialItem1);
@@ -68,10 +69,11 @@ public class IntroActivity extends AppCompatActivity {
 
         return tutorialItems;
     }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         //    super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == RESULT_OK && requestCode == REQUEST_CODE){
+        if (resultCode == RESULT_OK && requestCode == REQUEST_CODE) {
             prefManager.setFirstTimeLaunch(true);
             startActivity(new Intent(IntroActivity.this, LoginActivity.class));
             finish();
